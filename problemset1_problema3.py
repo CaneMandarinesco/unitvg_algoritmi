b6c3c0f69002e343dcef5d0210e4e5731f6eb4d4
Davide Luci <lucidavide@hotmail.it>
2024-12-05 23:40:31 +0100

    problema 3 ver. 2

--- problemset1_problema3.py
+++ problemset1_problema3.py
@@ -1,129 +1,94 @@
-import math
 
-# ogni nodo contiene:
-# * inizio
-# * fine
-# * numero_cfu
-# * indice di s1
-# * indice di s2
+# variabili globali
+nodi = 0
+T = []
+j = 0
 
-def figlioDestro(i, n):
-    if (i*2) + 1 <= n:
-        return (i*2) + 1
-    return -1
-
-def figlioSinistro(i, n):
-    if (i*2) <= n:
+def figlioSx(i):
+    if (i*2) <= nodi:
         return (i*2)
     return -1
 
-def max_cfu(s1, s2):
-    s1_s, s1_t, s1_v, s1_k = s1
-    s2_s, s2_t, s2_v, s2_k = s2
+def figlioDx(i):
+    if (i*2) + 1 <= nodi:
+        return (i*2) + 1
+    return -1
 
-    s1_len = s1_t - s1_s + 1
-    s2_len = s2_t - s2_s + 1
+def unisci_soluzioni(S, i, n, k):
+    # TODO: ora pero' devo iterare su altre liste...
+    s1_s, s1_t, s1_v, s1_k, _ = s1 = T[figlioSx(i)]
+    s2_s, s2_t, s2_v, s2_k, _ = s2 = T[figlioDx(i)]
 
-    # se le sequenze sono su corsie diverse
-    if (s1_k != s2_k):
-        # se s1 e s2 non si sovrappongono... easy!
-        if s1_t < s2_s:
-            return (s1_s, s2_t, (s1_len*s1_v) + (s2_len)*s2_v, 0, 0)
-
-        # se s1 e s2 si sovrappongono...
-        # se s1 contiene s2
-        if s1_t >= s2_t:
-            # se i cfu in s1 valgono piu di s2
-            if s1_v >= s2_v:
-                return (s1_s, s1_t, s1_len*s1_v, 0, 0)
-            # se i cfu in s1 valgono di meno
-            return (s1_s, s1_t, ((s1_len-s2_len)*s1_v) + (s2_len * s2_v), 0, 0)
+    if s1_k != s2_k:
+        # s2 compreso in s1?
+        if s1_t > s2_t:
+            if s1_v > s2_v:
+                return s1
+            
+            return  (s1_s,   s2_s-1, s1_v, s1_k, 
+                    (s2_s,   s2_t,   s2_v, s2_k, 
+                    (s2_t+1, s1_t,   s1_v, s1_k, None)))
+        # s2 e s1 terminano nello stesso t?
+        if s1_t == s2_t:
+            if s1_v > s2_v:
+                return s1
+            
+            return  (s1_s, s2_s-1, s1_v, s1_k, 
+                    (s2_s, s2_t,   s2_v, s2_k, None))
         
-        # se una parte di s2 si trova in s1 e l'altra e' fuori...
-        # se i cfu in s1 valgono piu di s2
-        if s1_v >= s2_v:
-            return (s1_s, s2_t, (s1_len * s_v) + ((s2_t - s1_t) * s2_v), 0, 0)
-        
-        # se i cfu in s1 valgono meno di s2
-        return (s1_s, s2_t, ((s2_s - s1_s) * s1_v) + (s2_len*s2_v), 0, 0)
-    
-    # se sono sulla stessa corsia, non si sovrappongono
-    return (s1_s, s_end, (s1_len*s1_v)+(s2_len*s2_v), 0, 0)
+        # s2 termina dopo s1?
+        if s1_t < s2_t:
+            if s1_v > s2_v:
+                return  (s1_s, s1_t, s1_v, s1_k,
+                        (s1_t+1, s2_t, s2_v, s2_k, None))
+            return  (s1_s, s2_s-1, s1_v, s1_k, 
+                    (s2_s, s2_t,   s2_v, s2_k, None))
 
-def unisici_soluzioni(T, S, i, n, k):
-    sx = figlioSinistro(i, n)
-    dx = figlioDestro(i, n)
+def tree_calcola_max_score(S,i,n,k):
+    """_summary_
+    Calcola il massimo score ottenibile in una sequenza S sfruttando 
+    la struttura dati ad albero binario contenuta in T
+    Args:
+        S (List): sequenze di monete
+        i (int): nodo da usare come radice
+        n (int): numero di sequenze
+        k (int): numero di corsie
+    """
+    global T, nodi, j
 
-    print(f"nodo {i}, devo unire le soluzioni: {T[sx]} e {T[dx]}")
+    sx = figlioSx(i)
+    dx = figlioDx(i)
 
-    if sx == -1 or dx == -1:
-        print('diocane errore!')
-    
-    return ()
-
-
-# variabile globale per tenere conto della sequenza corrente
-j = 0
-# variabile globale per tenere il numero di nodi
-nodi = 0
-def calcola_cfu(T, S, i, n, k):
-    global j, nodi
-    # T: albero binario
-    # S: sequenza in input
-    # i: indice del nodo per cui devo calcolare i cfu
-    # n: numero di sequenze
-    # k: corsie
-
-    sx = figlioSinistro(i, nodi)
-    dx = figlioDestro(i, nodi)
-
-    if (dx == -1 and sx == -1):
+    if dx == sx == -1:
         # -- CASO BASE --
+        T[i] = (*S[j], None)
+        j+=1
 
-        if(j+1 < n):
-            # lavoro su due sequenze
-            T[i] = max_cfu(S[j], S[j+1])
-            T[i][3] = j
-            T[i][4] = j+1
-        else:
-            # lavoro su una sola sequenza
-            s = S[j]
-            #      (start, fine, valore, s1, s2)
-            T[i] = (s[0], s[1], (s[1]-s[0] + 1) * s[2], j, -1)
-
-        j += 2
     else:
         # -- CASO RICORSIVO --
-        T = calcola_cfu(T, S, sx, n, k)
-        T = calcola_cfu(T, S, dx, n, k)
+        tree_calcola_max_score(S, sx, n ,k)
+        tree_calcola_max_score(S, dx, n, k)
 
         # metti insieme le soluzioni
-        s = unisici_soluzioni(T, S, i, n, k)
+        T[i] = unisci_soluzioni(S, i, n, k)
 
-    return T
+def alg_calcola_max_score(S,n,k):
+    """_summary_
+    Calcola il massimo score ottenibile in una sequenza S
+    Args:
+        S (List): sequenze di monete
+        n (int): numero di sequenze
+        k (int): numero di corsie
+    """
+    global T, nodi, v
+    nodi = (2*n)-1
+    v = 0
+    T = [(0,0,0,0,None) for _ in range(0,nodi+1)]
 
-def algoritmo(S, n, k):
-    # resetta sempre la variabile globale!
-    global j, nodi
-    j = 0
-
-    # T di grandezza n+1. la radice si trova in posizione 1
-    # T[i] = (inizio, fine, cfu)
-
-    # O(n)
-    T = []
-    nodi = n-1 if n%2 == 0 else n
-    for i in range(0,nodi+1):
-        T.append((0,0,0))
-
-    T = calcola_cfu(T, S, 1, n, k)
-
+    tree_calcola_max_score(S,1,n,k)
     print(T)
 
-    return T[1]
-
-# S[i] = (s: start, t: fine, v: valore, k: corsia)
 S = [(2,7,4,1), (3,3,9,3), (6,9,2,2), (7,8,5,3), (10,10,5,1), (10,10,1,3)]
 n = len(S)
 k = 3
-algoritmo(S,n,k)
+alg_calcola_max_score(S,n,k)
---
+++ problemset1_problema3_old.py
@@ -0,0 +1,131 @@
+import math
+
+# ogni nodo contiene:
+# * inizio
+# * fine
+# * numero_cfu
+# * indice di s1
+# * indice di s2
+
+def figlioDestro(i, n):
+    # O(1)
+    if (i*2) + 1 <= n:
+        return (i*2) + 1
+    return -1
+
+def figlioSinistro(i, n):
+    # O(1)
+    if (i*2) <= n:
+        return (i*2)
+    return -1
+
+def nodoToSequenza(i, n):
+    # O(1)
+    if(n%2 == 0):
+        return 
+
+def max_cfu(s1, s2):
+    # La procedura esegue in tempo O(1)
+    s1_s, s1_t, s1_v, s1_k = s1
+    s2_s, s2_t, s2_v, s2_k = s2
+
+    s1_len = s1_t - s1_s + 1
+    s2_len = s2_t - s2_s + 1
+
+    # se le sequenze sono su corsie diverse
+    if (s1_k != s2_k):
+        # se s1 e s2 non si sovrappongono... easy!
+        if s1_t < s2_s:
+            return (s1_s, s2_t, (s1_len*s1_v) + (s2_len)*s2_v)
+
+        # se s1 e s2 si sovrappongono...
+        # se s1 contiene s2
+        if s1_t >= s2_t:
+            # se i cfu in s1 valgono piu di s2
+            if s1_v >= s2_v:
+                return (s1_s, s1_t, s1_len*s1_v)
+            # se i cfu in s1 valgono di meno
+            return (s1_s, s1_t, ((s1_len-s2_len)*s1_v) + (s2_len * s2_v))
+        
+        # se una parte di s2 si trova in s1 e l'altra e' fuori...
+        # se i cfu in s1 valgono piu di s2
+        if s1_v >= s2_v:
+            return (s1_s, s2_t, (s1_len * s1_v) + ((s2_t - s1_t) * s2_v))
+        
+        # se i cfu in s1 valgono meno di s2
+        return (s1_s, s2_t, ((s2_s - s1_s) * s1_v) + (s2_len*s2_v))
+    
+    # se sono sulla stessa corsia, non si sovrappongono
+    return (s1_s, s2_t, (s1_len*s1_v)+(s2_len*s2_v))
+
+def unisici_soluzioni(T, S, i, n, k):
+    sx = figlioSinistro(i, n)
+    dx = figlioDestro(i, n)
+
+    print(f"nodo {i}, devo unire le soluzioni: {T[sx]} e {T[dx]}")
+
+    # mi basta iterare sulle n soluzioni!
+
+    if sx == -1 or dx == -1:
+        print('diocane errore!')
+        return (-1,-1,-1)
+    
+    
+
+# variabile globale per tenere conto della sequenza corrente
+j = 0
+# variabile globale per tenere il numero di nodi
+nodi = 0
+def calcola_cfu(T, S, i, n, k):
+    global j, nodi
+    # T: albero binario
+    # S: sequenza in input
+    # i: indice del nodo per cui devo calcolare i cfu
+    # n: numero di sequenze
+    # k: corsie
+
+    sx = figlioSinistro(i, nodi)
+    dx = figlioDestro(i, nodi)
+
+    if (dx == -1 and sx == -1):
+        # -- CASO BASE --
+
+        if(j+1 < n):
+            # lavoro su due sequenze
+            T[i] = max_cfu(S[j], S[j+1])
+        j += 1
+    else:
+        # -- CASO RICORSIVO --
+        T = calcola_cfu(T, S, sx, n, k)
+        T = calcola_cfu(T, S, dx, n, k)
+
+        # metti insieme le soluzioni
+        s = unisici_soluzioni(T, S, i, n, k)
+
+    return T
+
+def algoritmo(S, n, k):
+    # resetta sempre la variabile globale!
+    global j, nodi
+    j = 0
+
+    # T di grandezza n+1. la radice si trova in posizione 1
+    # T[i] = (inizio, fine, cfu)
+
+    # O(n)
+    T = []
+    nodi = n-1 if n%2 == 0 else n
+    for i in range(0,nodi+1):
+        T.append((0,0,0))
+
+    T = calcola_cfu(T, S, 1, n, k)
+
+    print(T)
+
+    return T[1]
+
+# S[i] = (s: start, t: fine, v: valore, k: corsia)
+S = [(2,7,4,1), (3,3,9,3), (6,9,2,2), (7,8,5,3), (10,10,5,1), (10,10,1,3)]
+n = len(S)
+k = 3
+algoritmo(S,n,k)
