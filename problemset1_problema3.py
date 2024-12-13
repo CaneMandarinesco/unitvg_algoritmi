7f09da10874d09dce111c3c89f1ff8db672d1c28
Davide Luci <lucidavide@hotmail.it>
2024-12-13 11:47:41 +0100

    asdasdf

--- problemset1_problema3.py
+++ problemset1_problema3.py
@@ -1,94 +1,63 @@
+def start(s):
+    return s[0]
 
-# variabili globali
-nodi = 0
-T = []
-j = 0
+def end(s):
+    return s[1]
 
-def figlioSx(i):
-    if (i*2) <= nodi:
-        return (i*2)
-    return -1
+def lens(s):
+    return (s[1] - s[0]) + 1
 
-def figlioDx(i):
-    if (i*2) + 1 <= nodi:
-        return (i*2) + 1
-    return -1
+def value(s):
+    return s[2]
 
-def unisci_soluzioni(S, i, n, k):
-    # TODO: ora pero' devo iterare su altre liste...
-    s1_s, s1_t, s1_v, s1_k, _ = s1 = T[figlioSx(i)]
-    s2_s, s2_t, s2_v, s2_k, _ = s2 = T[figlioDx(i)]
+def calcola_max_score(S,n,k):
+    X = S
+    S.sort(key= lambda s: value(s), reverse=True)
+    
+    occup = (0,0)
+    empty = (0,0)
+    for i in range(0,n,2):
+        score = 0
+        # proprieta: value(s1) >= value(s2) 
+        s1, s2 = (S[i], S[i+1])
 
-    if s1_k != s2_k:
-        # s2 compreso in s1?
-        if s1_t > s2_t:
-            if s1_v > s2_v:
-                return s1
-            
-            return  (s1_s,   s2_s-1, s1_v, s1_k, 
-                    (s2_s,   s2_t,   s2_v, s2_k, 
-                    (s2_t+1, s1_t,   s1_v, s1_k, None)))
-        # s2 e s1 terminano nello stesso t?
-        if s1_t == s2_t:
-            if s1_v > s2_v:
-                return s1
-            
-            return  (s1_s, s2_s-1, s1_v, s1_k, 
-                    (s2_s, s2_t,   s2_v, s2_k, None))
-        
-        # s2 termina dopo s1?
-        if s1_t < s2_t:
-            if s1_v > s2_v:
-                return  (s1_s, s1_t, s1_v, s1_k,
-                        (s1_t+1, s2_t, s2_v, s2_k, None))
-            return  (s1_s, s2_s-1, s1_v, s1_k, 
-                    (s2_s, s2_t,   s2_v, s2_k, None))
+        # Trova quante monete si sovrapp
 
-def tree_calcola_max_score(S,i,n,k):
-    """_summary_
-    Calcola il massimo score ottenibile in una sequenza S sfruttando 
-    la struttura dati ad albero binario contenuta in T
-    Args:
-        S (List): sequenze di monete
-        i (int): nodo da usare come radice
-        n (int): numero di sequenze
-        k (int): numero di corsie
-    """
-    global T, nodi, j
+        # Calcola il massimo per la coppia di sequenze
+        score += lens(s1) * value(s1)
+        # s1 contiene s2
+        if start(s2) >= start(s1) and end(s2) <= end(s1):
+            pass
+        # s2 contiene s1
+        elif start(s1) >= start(s2) and end(s1) <= end(s2):
+            score += (start(s1) - start(s2)) * value(s2)
+            score += (end(s2) - end(s1)) * value(s2)
+        # s1 supera s2
+        elif start(s2) <= end(s1) <= end(s2) and end(s2) > end(s1):
+            score += (start(s1) - start(s2)) * value(s2)
+        # s2 supera s1
+        elif start(s1) <= end(s2) <= end(s1) and end(s1) > end(s2):
+            score += (end(s2) - end(s1)) * value(s2)
+        # s1 e s2 consecutivi o ci sono spazi vuoti tra loro
+        else:
+            score += lens(s2) * value(s2)
 
-    sx = figlioSx(i)
-    dx = figlioDx(i)
+            # s1 subito dopo/prima s2?
+            if end(s1)+1 == start(s2) or end(s2)+1 == start(s1):
+                empty = (0,0)
+            elif end(s1)+1 <= start(s2)-1:
+                empty = (end(s1)+1, start(s2)-1)
+            elif end(s2)+1 <= start(s1)-1:
+                empty = (end(s2)+1, start(s1)-1)
 
-    if dx == sx == -1:
-        # -- CASO BASE --
-        T[i] = (*S[j], None)
-        j+=1
-
-    else:
-        # -- CASO RICORSIVO --
-        tree_calcola_max_score(S, sx, n ,k)
-        tree_calcola_max_score(S, dx, n, k)
-
-        # metti insieme le soluzioni
-        T[i] = unisci_soluzioni(S, i, n, k)
-
-def alg_calcola_max_score(S,n,k):
-    """_summary_
-    Calcola il massimo score ottenibile in una sequenza S
-    Args:
-        S (List): sequenze di monete
-        n (int): numero di sequenze
-        k (int): numero di corsie
-    """
-    global T, nodi, v
-    nodi = (2*n)-1
-    v = 0
-    T = [(0,0,0,0,None) for _ in range(0,nodi+1)]
-
-    tree_calcola_max_score(S,1,n,k)
-    print(T)
+        print(s1,s2, score, empty)
 
 S = [(2,7,4,1), (3,3,9,3), (6,9,2,2), (7,8,5,3), (10,10,5,1), (10,10,1,3)]
-n = len(S)
+n = 6
 k = 3
-alg_calcola_max_score(S,n,k)
+#calcola_max_score(S,n,k)
+
+S = [(1,3,2,1), (1,3,9,3), (3,5,10,2), (7,11,6,3), (9,9,9,2), (13,14,3,1)]
+n = 6
+k = 3
+calcola_max_score(S,n,k)
